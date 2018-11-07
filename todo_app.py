4ab7a8cb9276872846e81ee397e551bee9c5f704
Chung Daul <chungdaul@h0901mac08-1.local>
2018-11-07 11:55:27 +0900

    a simple post api for testing

---
+++ todo_app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/')
+def hello():
+    return 'hello'
+
+class CreateUser(Resource):
+    def post(self):
+        return {'status': 'success'}
+
+api.add_resource(CreateUser, '/user')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8100, debug=True)
