ca4ce19e5c78586c5c4b636af93bcdbbc947c045
Chung Daul <chungdaul@h0901mac08-1.local>
2018-11-07 13:08:01 +0900

    fixed ipaddress

--- todo_app.py
+++ todo_app.py
@@ -15,4 +15,7 @@ class CreateUser(Resource):
 api.add_resource(CreateUser, '/user')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8100, debug=True)
+    #app.run(host='127.0.0.1', port=8100, debug=True)
+    app.run()
+
+app.run()
