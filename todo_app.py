a23d84a73c81e1cd7d595a5e4731444925bea7d0
Chung Daul <chungdaul@h0901mac08-1.local>
2018-11-07 17:47:19 +0900

    modified response of API

--- todo_app.py
+++ todo_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_restful import reqparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,11 +9,12 @@ api = Api(app)
 def hello():
     return 'hello'
 
-class CreateUser(Resource):
-    def post(self):
-        return {"text": "yes my lord", "username": "daul"}
+class Listener(Resource):
+    #To do : Try and exception
+    def post(self, **kwargs):
+        return {"text": "yes my lord"+kwargs}
 
-api.add_resource(CreateUser, '/user')
+api.add_resource(Listener, '/listener')
 
 if __name__ == '__main__':
     #app.run(host='127.0.0.1', port=8100, debug=True)
