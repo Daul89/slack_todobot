1beab5d62181e703085f960c99134efeba0eb8ca
Chung Daul <chungdaul@h0901mac08-1.local>
2018-11-07 20:32:19 +0900

    modified response of API

--- todo_app.py
+++ todo_app.py
@@ -14,12 +14,32 @@ class Listener(Resource):
     #To do : Try and exception
     def post(self):
         
-        return {"text": json.dumps(request.form)}
+        indicator = request.form.get('text')
+        if(indicator == 'list'):
+            api_call = Listener2()
+            api_call.post()
 
-    def get(self, **kwargs):
-        return {"text": json.dumps(kwargs)}
+        #return {"text": json.dumps(request.form)}
+"""
+request.form has follwing dictionary
+
+{"token": "51Ep36kP6jZ0owNweBV7EfZ2", "team_id": "T0T9YUDFA", 
+"team_domain": "gcm-infra", "service_id": "473395518851", 
+"channel_id": "CDWNAEGSC", "channel_name": "bot-test", 
+"timestamp": "1541589611.006100", "user_id": "UDT51DG3V", 
+"user_name": "daul.chung", "text": "list", "trigger_word": "list"}
+"""
 
 api.add_resource(Listener, '/listener')
+
+
+class Listener2(Resource):
+    #To do : Try and exception
+    def post(self):
+
+        return {"text": "hello universe"}
+
+api.add_resource(Listener2, '/listener2')
 
 if __name__ == '__main__':
     #app.run(host='127.0.0.1', port=8100, debug=True)
