9a6c2ba8f3eda300e7754eb915ad104ca0ef2624
Chung Daul <chungdaul@h0901mac08-1.local>
2018-11-07 20:19:36 +0900

    modified response of API

--- todo_app.py
+++ todo_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_restful import reqparse
 import json
@@ -12,25 +12,14 @@ def hello():
 
 class Listener(Resource):
     #To do : Try and exception
-    def post(self, **kwargs):
+    def post(self):
         
-        return {"text": json.dumps(kwargs)}
+        return {"text": json.dumps(request.form)}
 
     def get(self, **kwargs):
         return {"text": json.dumps(kwargs)}
 
 api.add_resource(Listener, '/listener')
-
-class Listener2(Resource):
-    #To do : Try and exception
-    def post(self, **kwargs):
-        
-        return {"text": json.dumps(kwargs)}
-
-    def get(self, **kwargs):
-        return {"text": json.dumps(kwargs)}
-
-api.add_resource(Listener2, '/listener2')
 
 if __name__ == '__main__':
     #app.run(host='127.0.0.1', port=8100, debug=True)
